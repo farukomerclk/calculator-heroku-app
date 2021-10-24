af28e6e2d62d7d965986f163c1b167cb16ab0525
ömer faruk çelik <farukomer7000@gmail.com>
2021-10-24 15:27:59 +0300

    v2

--- client/main.js
+++ client/main.js
@@ -6,16 +6,9 @@ form.addEventListener('submit', function(e) {
   var num1 = document.querySelector('#num1');
   var num2 = document.querySelector('#num2');
   var result = document.querySelector('#result');
-  
 
-  console.log(num1);
-  console.log(num2);
-  console.log(num1.value);
-  console.log(num2.value);
-  console.log(result);
-  console.log(result.value);
 
-  result = num1.value + num2.value;
+  result.value = num1.value + num2.value;
   
   num1.value = '';
   num2.value = '';
